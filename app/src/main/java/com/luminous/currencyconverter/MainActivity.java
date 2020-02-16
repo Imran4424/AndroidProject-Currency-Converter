b359737c8f22e265f2b4bbb18c2cad104a1bfd74
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-02-16 13:06:45 +0600

    setting dropdown list

--- app/src/main/java/com/luminous/currencyconverter/MainActivity.java
+++ app/src/main/java/com/luminous/currencyconverter/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        spinnerOne =  findViewById(R.id.spinnerOne);
+        spinnerTwo =  findViewById(R.id.spinnerTwo);
     }
 }
