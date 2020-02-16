55cbcfe42e6811d2c5b744ef9ddc5858c276c91b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-02-16 13:04:21 +0600

    setting dropdown list

--- app/src/main/java/com/luminous/currencyconverter/MainActivity.java
+++ app/src/main/java/com/luminous/currencyconverter/MainActivity.java
@@ -3,12 +3,17 @@ package com.luminous.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    Spinner spinnerOne;
+    Spinner spinnerTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
     }
 }
